f59cb5b40a2268ea435fe68f74b9bbe8ec636cd4
yeaung00 <yep.aung00@gmail.com>
2022-06-08 19:55:25 -0700

    Fixed confetti

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const subscribeBtn = document.querySelector('button');
 
 subscribeBtn.addEventListener('click', (e) => {
-    x1 = 149 / getX(e);
-    y1 = 400.4 / getY(e);
-    console.log(e);
     confetti();
 });
